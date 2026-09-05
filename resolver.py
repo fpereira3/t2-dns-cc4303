f385046b87a375fa9c5117379e22282f7c56abae
FPereiraI <fabian.pereira.i@ug.uchile.cl>
2026-09-05 16:41:32 -0400

    feat: add dns response cache

--- resolver.py
+++ resolver.py
@@ -1,12 +1,17 @@
 import socket
 import dnslib
 from dnslib import DNSRecord, QTYPE
+from collections import deque, Counter
 
-IP_VM = "127.0.0.1"
+vm_ip = "127.0.0.1"
 
-buf_size = 4096
+buffer_size = 4096
 root_ip = "198.41.0.4"
 debug_mode = True
+
+query_history = deque(maxlen=20)
+cache = {}
+
 
 def parse_dns_message(dnslib_reply):
     dnslib_reply = DNSRecord.parse(dnslib_reply)
@@ -36,39 +41,72 @@ def parse_dns_message(dnslib_reply):
     return message
 
 def print_parsed_msg(msg):
-    print("MENSAJE DNS:")
+    print("DNS MESSAGE:")
     print("============================================================")
     for (key, value) in msg.items():
         print("{}: {}".format(key, value))
     print("============================================================\n")
 
-def print_debug(domain_name, nombre_ns, ip_addr):
+def print_debug(domain_name, name_server, ip_addr):
     if debug_mode:
-        print("(debug) Consultando '{}' a '{}' con dirección IP '{}'".format(domain_name, nombre_ns, ip_addr))
+        print("(debug) Consultando '{}' a '{}' con dirección IP '{}'".format(domain_name, name_server, ip_addr))
+
+def print_debug_cache(domain_name):
+    if debug_mode:
+        print("(debug) '{}' encontrado en caché, respondiendo directamente sin consultar Name Servers".format(domain_name))
+
+def update_history(domain_name):
+    query_history.append(domain_name)
+    counts = Counter(query_history)
+    top_3 = [domain for domain, _ in counts.most_common(3)]
+    return top_3
+
+def build_cached_response(query_message_bytes, answer_records):
+    parsed_query = DNSRecord.parse(query_message_bytes)
+    reply = parsed_query.reply()
+    for rr in answer_records:
+        reply.add_answer(rr)
+    return reply.pack()
 
 def recv_dns_msg(address, port):
-    sv_address = (address, port)
+    server_address = (address, port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    sock.bind(sv_address)
+    sock.bind(server_address)
 
     try:
         while True:
-            data, client_address = sock.recvfrom(buf_size)
+            data, client_address = sock.recvfrom(buffer_size)
             parsed_msg = parse_dns_message(data)
             print_parsed_msg(parsed_msg)
 
-            response = resolver(data)
+            domain_name = str(parsed_msg["qname"])
+            top_3 = update_history(domain_name)
+
+            if domain_name in cache:
+                print_debug_cache(domain_name)
+                response = build_cached_response(data, cache[domain_name])
+            else:
+                response = resolver(data)
+
+                if response is not None and domain_name in top_3:
+                    parsed_response = DNSRecord.parse(response)
+                    cache[domain_name] = parsed_response.rr
+
+            for cached_domain in list(cache.keys()):
+                if cached_domain not in top_3:
+                    del cache[cached_domain]
+
             if response is not None:
                 sock.sendto(response, client_address)
     finally:
         sock.close()
  
-def send_query(msg_consulta_b, ip_addr, port=53):
+def send_query(query_message_bytes, ip_addr, port=53):
     server_address = (ip_addr, port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        sock.sendto(msg_consulta_b, server_address)
+        sock.sendto(query_message_bytes, server_address)
         data, _ = sock.recvfrom(4096)
     finally:
         sock.close()
@@ -76,8 +114,8 @@ def send_query(msg_consulta_b, ip_addr, port=53):
 
 
 def resolver(mensaje_consulta, ip_addr=root_ip, nombre_ns="."):
-    parsed_consulta = parse_dns_message(mensaje_consulta)
-    domain_name = str(parsed_consulta["qname"])
+    parsed_query = parse_dns_message(mensaje_consulta)
+    domain_name = str(parsed_query["qname"])
 
     print_debug(domain_name, nombre_ns, ip_addr)
 
@@ -96,42 +134,42 @@ def resolver(mensaje_consulta, ip_addr=root_ip, nombre_ns="."):
 
     if number_of_authority_elements > 0:
         authority_section_list = dnslib_reply.auth
-        hay_ns_en_authority = any(QTYPE.get(rr.rtype) == 'NS' for rr in authority_section_list)
+        has_ns_in_authority = any(QTYPE.get(rr.rtype) == 'NS' for rr in authority_section_list)
 
-        if hay_ns_en_authority:
-            primer_ns = authority_section_list[0]
-            nombre_ns_siguiente = str(primer_ns.rdata)
+        if has_ns_in_authority:
+            first_ns = authority_section_list[0]
+            next_name_server = str(first_ns.rdata)
 
             additional_records = dnslib_reply.ar
-            ip_en_additional = None
+            ip_in_additional = None
             for additional_record in additional_records:
                 ar_type = QTYPE.get(additional_record.rtype)
                 if ar_type == 'A':
-                    ip_en_additional = str(additional_record.rdata)
+                    ip_in_additional = str(additional_record.rdata)
                     break
 
-            if ip_en_additional is not None:
-                return resolver(mensaje_consulta, ip_en_additional, nombre_ns_siguiente)
+            if ip_in_additional is not None:
+                return resolver(mensaje_consulta, ip_in_additional, next_name_server)
 
             else:
-                query_ns = DNSRecord.question(nombre_ns_siguiente)
-                respuesta_ns = resolver(query_ns.pack(), root_ip, ".")
+                ns_query = DNSRecord.question(next_name_server)
+                ns_response = resolver(ns_query.pack(), root_ip, ".")
  
-                if respuesta_ns is None:
+                if ns_response is None:
                     return None
  
-                dnslib_reply_ns = DNSRecord.parse(respuesta_ns)
-                ip_ns = None
-                for rr in dnslib_reply_ns.rr:
+                ns_parsed_reply = DNSRecord.parse(ns_response)
+                ns_ip = None
+                for rr in ns_parsed_reply.rr:
                     if QTYPE.get(rr.rtype) == 'A':
-                        ip_ns = str(rr.rdata)
+                        ns_ip = str(rr.rdata)
                         break
  
-                if ip_ns is not None:
-                    return resolver(mensaje_consulta, ip_ns, nombre_ns_siguiente)
+                if ns_ip is not None:
+                    return resolver(mensaje_consulta, ns_ip, next_name_server)
                 else:
                     return None
 
     return None
 
-recv_dns_msg(IP_VM, 8000)
+recv_dns_msg(vm_ip, 8000)
